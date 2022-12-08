90fb998f86c99680077a5b02888d5a6310bd03bb
peter <Han-D-Peter@github.com>
2022-12-08 16:48:23 +0900

    style: 컬러코드 추가

--- packages/common-styles/src/defaultTheme.ts
+++ packages/common-styles/src/defaultTheme.ts
@@ -1,35 +1,240 @@
+import * as colors from './colors';
 import { ITheme } from './types';
 
 const theme: ITheme = {
-  palette: {
+  colors: colors.default.colors,
+  btnSize: {
+    xs: {
+      width: 80,
+      height: 40,
+    },
+    sm: {
+      width: 90,
+      height: 45,
+    },
+    md: {
+      width: 100,
+      height: 50,
+    },
+    lg: {
+      width: 110,
+      height: 55,
+    },
+  },
+  inputSize: {
+    xs: {
+      height: 24,
+      borderRadius: 2,
+      fontSize: 12,
+    },
+    sm: {
+      height: 32,
+      borderRadius: 4,
+      fontSize: 15,
+    },
+    md: {
+      height: 40,
+      borderRadius: 6,
+      fontSize: 18,
+    },
+    lg: {
+      height: 48,
+      borderRadius: 8,
+      fontSize: 21,
+    },
+  },
+  btnActionColor: {
     gray: {
-      black: '#000000',
-      gray0: '#2d2d32',
-      gray1: '#444444',
-      gray2: '#666666',
-      gray3: '#999999',
-      gray4: '#bfbfbf',
-      gray5: '#dddddd',
-      gray6: '#f3f3f3',
-      gray7: '#f7f7f7',
-      white: '#ffffff',
+      basic: colors.default.colors.gray.gray7,
+      solid: {
+        normal: colors.default.colors.gray.gray7,
+        hover: colors.default.colors.gray.gray8,
+        active: colors.default.colors.gray.gray9,
+      },
+      outline: {
+        normal: colors.default.colors.gray.gray0,
+        hover: colors.default.colors.gray.gray3,
+        active: colors.default.colors.gray.gray5,
+      },
+      ghost: {
+        normal: colors.default.colors.gray.gray0,
+        hover: colors.default.colors.gray.gray3,
+        active: colors.default.colors.gray.gray5,
+      },
     },
-    fontWeight: {
-      thin: 100,
-      extraLight: 200,
-      light: 300,
-      regular: 400,
-      medium: 500,
-      semiBold: 600,
-      bold: 700,
-      extraBold: 800,
-      black: 900,
+    red: {
+      basic: colors.default.colors.red.red6,
+      solid: {
+        normal: colors.default.colors.red.red6,
+        hover: colors.default.colors.red.red8,
+        active: colors.default.colors.red.red9,
+      },
+      outline: {
+        normal: colors.default.colors.red.red0,
+        hover: colors.default.colors.red.red3,
+        active: colors.default.colors.red.red5,
+      },
+      ghost: {
+        normal: colors.default.colors.red.red0,
+        hover: colors.default.colors.red.red3,
+        active: colors.default.colors.red.red5,
+      },
     },
-    hue: {
-      mint: '#2ac1bc',
-      red: '#f45452',
-      gray: '#999999',
+    orange: {
+      basic: colors.default.colors.orange.orange7,
+      solid: {
+        normal: colors.default.colors.orange.orange7,
+        hover: colors.default.colors.orange.orange8,
+        active: colors.default.colors.orange.orange9,
+      },
+      outline: {
+        normal: colors.default.colors.orange.orange0,
+        hover: colors.default.colors.orange.orange3,
+        active: colors.default.colors.orange.orange5,
+      },
+      ghost: {
+        normal: colors.default.colors.orange.orange0,
+        hover: colors.default.colors.orange.orange3,
+        active: colors.default.colors.orange.orange5,
+      },
     },
+    yellow: {
+      basic: colors.default.colors.yellow.yellow7,
+      solid: {
+        normal: colors.default.colors.yellow.yellow7,
+        hover: colors.default.colors.yellow.yellow8,
+        active: colors.default.colors.yellow.yellow9,
+      },
+      outline: {
+        normal: colors.default.colors.yellow.yellow0,
+        hover: colors.default.colors.yellow.yellow3,
+        active: colors.default.colors.yellow.yellow5,
+      },
+      ghost: {
+        normal: colors.default.colors.yellow.yellow0,
+        hover: colors.default.colors.yellow.yellow3,
+        active: colors.default.colors.yellow.yellow5,
+      },
+    },
+    green: {
+      basic: colors.default.colors.green.green7,
+      solid: {
+        normal: colors.default.colors.green.green7,
+        hover: colors.default.colors.green.green8,
+        active: colors.default.colors.green.green9,
+      },
+      outline: {
+        normal: colors.default.colors.green.green0,
+        hover: colors.default.colors.green.green3,
+        active: colors.default.colors.green.green5,
+      },
+      ghost: {
+        normal: colors.default.colors.green.green0,
+        hover: colors.default.colors.green.green3,
+        active: colors.default.colors.green.green5,
+      },
+    },
+    teal: {
+      basic: colors.default.colors.teal.teal7,
+      solid: {
+        normal: colors.default.colors.teal.teal7,
+        hover: colors.default.colors.teal.teal8,
+        active: colors.default.colors.teal.teal9,
+      },
+      outline: {
+        normal: colors.default.colors.teal.teal0,
+        hover: colors.default.colors.teal.teal3,
+        active: colors.default.colors.teal.teal5,
+      },
+      ghost: {
+        normal: colors.default.colors.teal.teal0,
+        hover: colors.default.colors.teal.teal3,
+        active: colors.default.colors.teal.teal5,
+      },
+    },
+    blue: {
+      basic: colors.default.colors.blue.blue7,
+      solid: {
+        normal: colors.default.colors.blue.blue7,
+        hover: colors.default.colors.blue.blue8,
+        active: colors.default.colors.blue.blue9,
+      },
+      outline: {
+        normal: colors.default.colors.blue.blue0,
+        hover: colors.default.colors.blue.blue3,
+        active: colors.default.colors.blue.blue5,
+      },
+      ghost: {
+        normal: colors.default.colors.blue.blue0,
+        hover: colors.default.colors.blue.blue3,
+        active: colors.default.colors.blue.blue5,
+      },
+    },
+    cyan: {
+      basic: colors.default.colors.cyan.cyan7,
+      solid: {
+        normal: colors.default.colors.cyan.cyan7,
+        hover: colors.default.colors.cyan.cyan8,
+        active: colors.default.colors.cyan.cyan9,
+      },
+      outline: {
+        normal: colors.default.colors.cyan.cyan0,
+        hover: colors.default.colors.cyan.cyan3,
+        active: colors.default.colors.cyan.cyan5,
+      },
+      ghost: {
+        normal: colors.default.colors.cyan.cyan0,
+        hover: colors.default.colors.cyan.cyan3,
+        active: colors.default.colors.cyan.cyan5,
+      },
+    },
+    purple: {
+      basic: colors.default.colors.violet.violet7,
+      solid: {
+        normal: colors.default.colors.violet.violet7,
+        hover: colors.default.colors.violet.violet8,
+        active: colors.default.colors.violet.violet9,
+      },
+      outline: {
+        normal: colors.default.colors.violet.violet0,
+        hover: colors.default.colors.violet.violet3,
+        active: colors.default.colors.violet.violet5,
+      },
+      ghost: {
+        normal: colors.default.colors.violet.violet0,
+        hover: colors.default.colors.violet.violet3,
+        active: colors.default.colors.violet.violet5,
+      },
+    },
+    pink: {
+      basic: colors.default.colors.pink.pink7,
+      solid: {
+        normal: colors.default.colors.pink.pink7,
+        hover: colors.default.colors.pink.pink8,
+        active: colors.default.colors.pink.pink9,
+      },
+      outline: {
+        normal: colors.default.colors.pink.pink0,
+        hover: colors.default.colors.pink.pink3,
+        active: colors.default.colors.pink.pink5,
+      },
+      ghost: {
+        normal: colors.default.colors.pink.pink0,
+        hover: colors.default.colors.pink.pink3,
+        active: colors.default.colors.pink.pink5,
+      },
+    },
+  },
+  fontWeight: {
+    thin: 100,
+    extraLight: 200,
+    light: 300,
+    regular: 400,
+    medium: 500,
+    semiBold: 600,
+    bold: 700,
+    extraBold: 800,
+    black: 900,
   },
 };
 
--- packages/common-styles/src/types.ts
+++ packages/common-styles/src/types.ts
@@ -1,7 +1,6 @@
 export interface ITheme {
-  palette: {
+  colors: {
     gray: {
-      black: string;
       gray0: string;
       gray1: string;
       gray2: string;
@@ -10,23 +9,385 @@ export interface ITheme {
       gray5: string;
       gray6: string;
       gray7: string;
-      white: string;
+      gray8: string;
+      gray9: string;
     };
-    fontWeight: {
-      thin: number;
-      extraLight: number;
-      light: number;
-      regular: number;
-      medium: number;
-      semiBold: number;
-      bold: number;
-      extraBold: number;
-      black: number;
+    red: {
+      red0: string;
+      red1: string;
+      red2: string;
+      red3: string;
+      red4: string;
+      red5: string;
+      red6: string;
+      red7: string;
+      red8: string;
+      red9: string;
     };
-    hue: {
-      mint: string;
-      red: string;
-      gray: string;
+    pink: {
+      pink0: string;
+      pink1: string;
+      pink2: string;
+      pink3: string;
+      pink4: string;
+      pink5: string;
+      pink6: string;
+      pink7: string;
+      pink8: string;
+      pink9: string;
     };
+    grape: {
+      grape0: string;
+      grape1: string;
+      grape2: string;
+      grape3: string;
+      grape4: string;
+      grape5: string;
+      grape6: string;
+      grape7: string;
+      grape8: string;
+      grape9: string;
+    };
+    violet: {
+      violet0: string;
+      violet1: string;
+      violet2: string;
+      violet3: string;
+      violet4: string;
+      violet5: string;
+      violet6: string;
+      violet7: string;
+      violet8: string;
+      violet9: string;
+    };
+    indigo: {
+      indigo0: string;
+      indigo1: string;
+      indigo2: string;
+      indigo3: string;
+      indigo4: string;
+      indigo5: string;
+      indigo6: string;
+      indigo7: string;
+      indigo8: string;
+      indigo9: string;
+    };
+    blue: {
+      blue0: string;
+      blue1: string;
+      blue2: string;
+      blue3: string;
+      blue4: string;
+      blue5: string;
+      blue6: string;
+      blue7: string;
+      blue8: string;
+      blue9: string;
+    };
+    cyan: {
+      cyan0: string;
+      cyan1: string;
+      cyan2: string;
+      cyan3: string;
+      cyan4: string;
+      cyan5: string;
+      cyan6: string;
+      cyan7: string;
+      cyan8: string;
+      cyan9: string;
+    };
+    teal: {
+      teal0: string;
+      teal1: string;
+      teal2: string;
+      teal3: string;
+      teal4: string;
+      teal5: string;
+      teal6: string;
+      teal7: string;
+      teal8: string;
+      teal9: string;
+    };
+    green: {
+      green0: string;
+      green1: string;
+      green2: string;
+      green3: string;
+      green4: string;
+      green5: string;
+      green6: string;
+      green7: string;
+      green8: string;
+      green9: string;
+    };
+    lime: {
+      lime0: string;
+      lime1: string;
+      lime2: string;
+      lime3: string;
+      lime4: string;
+      lime5: string;
+      lime6: string;
+      lime7: string;
+      lime8: string;
+      lime9: string;
+    };
+    yellow: {
+      yellow0: string;
+      yellow1: string;
+      yellow2: string;
+      yellow3: string;
+      yellow4: string;
+      yellow5: string;
+      yellow6: string;
+      yellow7: string;
+      yellow8: string;
+      yellow9: string;
+    };
+    orange: {
+      orange0: string;
+      orange1: string;
+      orange2: string;
+      orange3: string;
+      orange4: string;
+      orange5: string;
+      orange6: string;
+      orange7: string;
+      orange8: string;
+      orange9: string;
+    };
+  };
+  btnSize: {
+    xs: {
+      width: number;
+      height: number;
+    };
+    sm: {
+      width: number;
+      height: number;
+    };
+    md: {
+      width: number;
+      height: number;
+    };
+    lg: {
+      width: number;
+      height: number;
+    };
+  };
+  inputSize: {
+    xs: {
+      height: number;
+      borderRadius: number;
+      fontSize: number;
+    };
+    sm: {
+      height: number;
+      borderRadius: number;
+      fontSize: number;
+    };
+    md: {
+      height: number;
+      borderRadius: number;
+      fontSize: number;
+    };
+    lg: {
+      height: number;
+      borderRadius: number;
+      fontSize: number;
+    };
+  };
+  btnActionColor: {
+    gray: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    red: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    orange: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    yellow: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    green: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    teal: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    blue: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    cyan: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    purple: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+    pink: {
+      basic: string;
+      solid: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      outline: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+      ghost: {
+        normal: string;
+        hover: string;
+        active: string;
+      };
+    };
+  };
+  fontWeight: {
+    thin: number;
+    extraLight: number;
+    light: number;
+    regular: number;
+    medium: number;
+    semiBold: number;
+    bold: number;
+    extraBold: number;
+    black: number;
   };
 }
