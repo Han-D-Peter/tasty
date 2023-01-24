ee6fff22f30b4dd477c48b9c48b858db991040c8
peter <Han-D-Peter@github.com>
2023-01-24 19:08:04 +0900

    feat: 드랍다운 기본 기능

---
+++ packages/common-components/src/hooks/useIsShown.ts
@@ -0,0 +1,17 @@
+import { useCallback, useState } from 'react';
+
+const useIsShown = (initialValue?: boolean) => {
+  const [isShown, setIsShown] = useState(initialValue ?? false);
+
+  const onOpen = useCallback(() => {
+    setIsShown(true);
+  }, []);
+
+  const onClose = useCallback(() => {
+    setIsShown(false);
+  }, []);
+
+  return { isShown, onOpen, onClose } as const;
+};
+
+export default useIsShown;
